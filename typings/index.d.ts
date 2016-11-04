211a68f42c2e69a908931a17f809d1df0848449d
Felipe Caputo <felipe.caputo@gmail.com>
2016-11-03 23:49:45 -0200

    added istanbul for coverage

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/chai/index.d.ts" />
+/// <reference path="globals/istanbul/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/sinon/index.d.ts" />
