b5ad4b27240f4b3cbe5f9e031149da8780230246
Felipe Caputo <felipe.caputo@gmail.com>
2016-11-03 23:50:06 -0200

    removeVertex and removeEdge

--- src/data-structures/graph.js
+++ src/data-structures/graph.js
@@ -21,14 +21,25 @@ class Graph {
      * @return {Object} if the vertex alredy exists returns the already inserted Vertex object, 
      * else it returns the new Vertex instance
      */
-    addVertex(vertex) {        
+    addVertex(vertex) {
         let aVertex = typeof vertex == 'string' ? new Vertex(vertex) : vertex;
-        if(this.findVertex(aVertex.name))
+        if (this.findVertex(aVertex.name))
             return this.vertices.get(aVertex.name); //Returns the already inserted instance of the vertex
 
         this.vertices.set(aVertex.name, aVertex);
         return aVertex;
-    }
+    };
+
+    /**
+     * Removes a vertex from graph
+     * 
+     * @param {string} vertexName
+     * @returns Boolean
+     */
+    removeVertex(vertexName) {
+        return this.vertices.delete(vertexName);
+    };
+
     /**
      * Check vertex existence in the Graph
      * 
@@ -36,41 +47,54 @@ class Graph {
      * @returns {Boolean} Returns true if the vertex already exists in the graph or false otherwise
      */
     findVertex(vertexName) {
-        return this.vertices.has(vertexName);
-    }
-    /**
-     * Returns an existing Vertex instance or undefined
-     * 
-     * @param {string} vertexName The name of the vertex to retrieve
-     * @return {Object|undefined}
-     */
-    getVertex(vertexName){
+            return this.vertices.has(vertexName);
+        }
+        /**
+         * Returns an existing Vertex instance or undefined
+         * 
+         * @param {string} vertexName The name of the vertex to retrieve
+         * @return {Object|undefined}
+         */
+    getVertex(vertexName) {
         return this.vertices.get(vertexName);
     }
 
     /**
-     * Add a new edge
+     * Adds a new edge
      * 
      * @param {string} source
      * @param {string} dest
      * @param {number} distance
-     * 
-     * @memberOf Graph
      */
     addEdge(source, dest, distance) {
-        if(this.findEdge(source, dest))  return;
+        if (this.findEdge(source, dest)) return;
 
         const srcVertex = this.getVertex(source);
         const destVertex = this.getVertex(dest);
-        if(typeof srcVertex == 'undefined' || typeof destVertex == 'undefined')
+        if (typeof srcVertex == 'undefined' || typeof destVertex == 'undefined')
             throw `${source||dest} is not part of this graph`;
         this.edges.set(`${source}-${dest}`, new VertexEdge(srcVertex, destVertex, distance));
     }
 
+    /**
+     * Returns a {Boolean} indicating if an edge exists or not
+     * 
+     * @param {string} source the name of the source vertex
+     * @param {string} dest
+     * @returns
+     * 
+     * @memberOf Graph
+     */
     findEdge(source, dest) {
-        return this.edges.has(`${source}-${dest}`)
-            || this.edges.has(`${dest}-${source}`)
+        return this.edges.has(`${source}-${dest}`) ||
+            this.edges.has(`${dest}-${source}`)
     }
+
+    removeEdge(source, dest) {
+        return this.edges.delete(`${source}-${dest}`) ||
+            this.edges.delete(`${dest}-${source}`)
+    }
+
 }
 
 module.exports = Graph
--- test/graph-test.js
+++ test/graph-test.js
@@ -4,7 +4,7 @@ const expect = require('chai').expect;
 
 describe('Graph', () => {
     let graph = new Graph();;
-    beforeEach( () => {
+    beforeEach(() => {
         graph = new Graph();
     })
     describe('Vertices', () => {
@@ -14,11 +14,11 @@ describe('Graph', () => {
                 expect(graph.vertices.size).to.be.eql(1);
                 graph.addVertex('2');
                 expect(graph.vertices.size).to.be.eql(2);
-            }); 
+            });
 
             it('should return the added vertex', () => {
                 expect(graph.addVertex('1')).to.be.instanceOf(Vertex);
-            });           
+            });
 
             it('Should not add repeated vertices', () => {
                 const vertex = graph.addVertex('1');
@@ -41,32 +41,103 @@ describe('Graph', () => {
                 expect(graph.findVertex('1')).to.be.false;
                 graph.addVertex('1');
                 expect(graph.findVertex('2')).to.be.false;
-            });            
+            });
         });
+
+        describe('removeVertex', () => {
+            beforeEach(() => {
+                graph.addVertex('1');
+                graph.addVertex('2');
+            });
+
+            it('should remove a vertex', () => {
+                expect(graph.vertices.size).to.be.equal(2);
+                expect(graph.removeVertex('1')).to.be.true;
+                expect(graph.vertices.size).to.be.equal(1);
+            });
+
+            it('should return false on a non existing vertex', () => {
+                expect(graph.removeVertex('3')).to.be.false;
+            });
+        });
+
     })
 
     describe('Edges tests', () => {
-        beforeEach( () => {
+        beforeEach(() => {
             graph.addVertex('1');
             graph.addVertex('2');
             graph.addVertex('3');
             graph.addVertex('4');
         })
-        it('should add edges', () => {
-            graph.addEdge('1', '2', 1);
-            expect(graph.edges.size).to.be.eql(1);
-            graph.addEdge('2', '3', 2);
-            expect(graph.edges.size).to.be.eql(2);
+
+        describe('addEdge', () => {
+            it('should add edges', () => {
+                graph.addEdge('1', '2', 1);
+                expect(graph.edges.size).to.be.eql(1);
+                graph.addEdge('2', '3', 2);
+                expect(graph.edges.size).to.be.eql(2);
+            });
+
+            it('should not add repeated edges', () => {
+                graph.addEdge('1', '2', 1);
+                expect(graph.edges.size).to.be.eql(1);
+                graph.addEdge('1', '2', 1);
+                expect(graph.edges.size).to.be.eql(1);
+                graph.addEdge('2', '1', 1);
+                expect(graph.edges.size).to.be.eql(1);
+            });
         });
 
-        it('should not add repeated edges', () => {
-            graph.addEdge('1', '2', 1);
-            expect(graph.edges.size).to.be.eql(1);
-            graph.addEdge('1', '2', 1);
-            expect(graph.edges.size).to.be.eql(1);
-            graph.addEdge('2', '1', 1);
-            expect(graph.edges.size).to.be.eql(1);
+        describe('findEdge', () => {
+            beforeEach(() => {
+                graph.addEdge('1', '2', 1);
+                graph.addEdge('2', '3', 1);
+            });
+
+            it('should find existing edges', () => {
+                expect(graph.findEdge('1', '2')).to.be.true;
+            });
+
+            it('should return false on non existing edge', () => {
+                expect(graph.findEdge('1', '3')).to.be.false;
+            })
+
+            it('should find reverse edges too', () => {
+                expect(graph.findEdge('2', '1')).to.be.true;
+            })
         });
+
+        describe('removeEdge', () => {
+            beforeEach(() => {
+                graph.addEdge('1', '2', 1);
+                graph.addEdge('2', '3', 1);
+            });
+
+            it('should remove existing edges', () => {
+                expect(graph.edges.size).to.be.equal(2);
+                graph.removeEdge('1', '2');
+                expect(graph.edges.size).to.be.equal(1);
+                graph.removeEdge('2', '3');
+                expect(graph.edges.size).to.be.equal(0);
+            });
+
+            it('should remove reverse edges too', () => {
+                expect(graph.edges.size).to.be.equal(2);
+                graph.removeEdge('2', '1');
+                expect(graph.edges.size).to.be.equal(1);
+                graph.removeEdge('3', '2');
+                expect(graph.edges.size).to.be.equal(0);
+            })
+
+            it('should return false if no edge exists', () => {
+                expect(graph.edges.size).to.be.equal(2);
+                graph.removeEdge('1', '3');
+                expect(graph.edges.size).to.be.equal(2);
+            })
+        });
+
+
     })
 
 })
